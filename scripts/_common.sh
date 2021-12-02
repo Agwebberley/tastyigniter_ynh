b7ba0cb10a54c3652ad2cf8a41588aaf0281b9e5
tituspijean <tituspijean@outlook.com>
2021-12-02 22:43:58 +0100

    Fix NGINX and PHP conf

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
-phpversion="7.4"
+YNH_PHP_VERSION="7.4"
+phpversion=$YNH_PHP_VERSION
 
 # dependencies used by the app
-pkg_dependencies="php${phpversion}-fpm php${phpversion}-mysql php${phpversion}-curl php${phpversion}-dom php${phpversion}-gd php${phpversion}-mbstring php${phpversion}-json php${phpversion}-tokenizer php${phpversion}-zip"
+pkg_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-zip"
 
 
 #=================================================
