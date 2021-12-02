2159f7e6fa733158f6a6cb772e6b58ce4abf36b9
tituspijean <tituspijean@outlook.com>
2021-12-02 21:47:50 +0100

    Make the app installable

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies=""
+phpversion="7.4"
 
-YNH_PHP_VERSION="7.4"
+# dependencies used by the app
+pkg_dependencies="php${phpversion}-fpm php${phpversion}-mysql php${phpversion}-curl php${phpversion}-dom php${phpversion}-gd php${phpversion}-mbstring php${phpversion}-json php${phpversion}-tokenizer php${phpversion}-zip"
+
 
 #=================================================
 # PERSONAL HELPERS
